b89f23b83c2e41abcd208bc2d33ba21b9cccb66b
Usuario <Usuario@CPD03-M10>
2024-02-27 11:53:46 -0300

    Adicionar arquivos de projeto.

---
+++ FrameWorkIdade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorkIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua idade em anos: ");
+            int Ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua idade em meses, sem considerar os anos: ");
+            int Meses = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua idade em dias, sem considerar os meses: ");
+            int Dia = int.Parse(Console.ReadLine());
+
+            int Idade = (Ano * 365) + (Meses * 30) + Dia;
+
+            Console.WriteLine("A sua idade em dias é de: " + Idade);
+
+            Console.ReadKey();
+        }
+
+    }
+}
